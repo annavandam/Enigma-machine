ee5b233222e1df9355425cfbb2edfa4e1dedf6fa
Wouter Paardekooper <110270@st-maartenscollege.nl>
2020-05-26 21:18:39 +0000

    Letters die door rotoren gaan is nu geschreven in een aparte functie waardoor de code overzichtelijker is

--- main.py
+++ main.py
@@ -8,6 +8,44 @@ class Rotor:
     self.stand = stand
     self.lettervolgorde = lettervolgorde
 
+#Functie die een rotor een aantal keer laat draaien.
+def RotorDraai(draaiingen, rotor):
+  rotor = (rotor.lettervolgorde[-draaiingen:] + rotor.lettervolgorde)[0: 26]
+  return rotor
+
+
+def LetterDoorRotoren(InputLetter):
+  if alfabet.find(InputLetter) == -1:
+    VertaaldeTekst.append(InputLetter)
+    return
+  #Gaat door rotor 1, 2 & 3.
+  InputLetter = rotor1.lettervolgorde[alfabet.index(InputLetter)]
+  InputLetter = rotor2.lettervolgorde[alfabet.index(InputLetter)]
+  InputLetter = rotor3.lettervolgorde[alfabet.index(InputLetter)]
+  #Alfabet omkeren.
+  InputLetter = alfabet[::-1][alfabet.index(InputLetter)]
+  #Opnieuw door de rotoren maar nu vanaf de andere kant benaderd.
+  InputLetter = alfabet[rotor3.lettervolgorde.index(InputLetter)]
+  InputLetter = alfabet[rotor2.lettervolgorde.index(InputLetter)]
+  InputLetter = alfabet[rotor3.lettervolgorde.index(InputLetter)]
+  VertaaldeTekst.append(InputLetter)
+
+  #Rotor 1 draait één tikje.
+  rotor1.lettervolgorde = RotorDraai(1, rotor1)
+  rotor1.stand += 1
+
+  #Als rotor 1 een volledige draai heeft gemaakt, draait rotor 2 één tikje.
+  if rotor1.stand % 26 == 0:
+    rotor1.stand = 0
+    rotor2.lettervolgorde = RotorDraai(1, rotor2)
+    rotor2.stand += 1
+    #Als rotor 2 een volledige draai heeft gemaakt, draait rotor 3 één tikje.
+    if rotor2.stand % 26 == 0:
+      rotor2.stand = 0
+      rotor3.lettervolgorde = RotorDraai(1, rotor3)
+      rotor3.stand += 1
+
+
 #De rotoren worden hier aangemaakt.
 rotor1 = Rotor(1, alfabet,  0)
 rotor2 = Rotor(2, alfabet,  0)
@@ -37,17 +75,12 @@ while True:
   except:
     print("Hier kunt u alleen getallen invoeren.")
 
-print("Voer de tekst in die u wilt omzetten.")
+print("Voer de tekst in die u wilt encrypten/decrypten.")
 #De tekst input die het moet worden gecodeerd. 
 TeVertalenString = input()
 
 #De TeVertalenString wordt een list.
 TeVertalenLijst = [char for char in TeVertalenString.lower()]
-
-#Functie die een rotor een aantal keer laat draaien.
-def RotorDraai(draaiingen, rotor):
-  rotor = (rotor.lettervolgorde[-draaiingen:] + rotor.lettervolgorde)[0: 26]
-  return rotor
 
 #De rotoren worden gedraaid naar hun standbeginstand
 rotor1.lettervolgorde = RotorDraai(rotor1.stand, rotor1)
@@ -55,37 +88,11 @@ rotor2.lettervolgorde = RotorDraai(rotor2.stand, rotor2)
 rotor3.lettervolgorde = RotorDraai(rotor3.stand, rotor3)
 
 
-
 #Hier gaan de letters daadwerkelijk door de rotoren:
 #Per letter wordt dit doorlopen.
 for x in TeVertalenLijst:
-  #Gaat door rotor 1, 2 & 3.
-  x = rotor1.lettervolgorde[alfabet.index(x)]
-  x = rotor2.lettervolgorde[alfabet.index(x)]
-  x = rotor3.lettervolgorde[alfabet.index(x)]
-  #Alfabet omkeren.
-  x = alfabet[::-1][alfabet.index(x)]
-  #Opnieuw door de rotoren maar nu vanaf de andere kant benaderd.
-  x = alfabet[rotor3.lettervolgorde.index(x)]
-  x = alfabet[rotor2.lettervolgorde.index(x)]
-  x = alfabet[rotor3.lettervolgorde.index(x)]
-  VertaaldeTekst.append(x)
-
-  #Rotor 1 draait één tikje.
-  rotor1.lettervolgorde = RotorDraai(1, rotor1)
-  rotor1.stand += 1
-
-  #Als rotor 1 een volledige draai heeft gemaakt, draait rotor 2 één tikje.
-  if rotor1.stand % 26 == 0:
-    rotor1.stand = 0
-    rotor2.lettervolgorde = RotorDraai(1, rotor2)
-    rotor2.stand += 1
-    #Als rotor 2 een volledige draai heeft gemaakt, draait rotor 3 één tikje.
-    if rotor2.stand % 26 == 0:
-      rotor2.stand = 0
-      rotor3.lettervolgorde = RotorDraai(1, rotor3)
-      rotor3.stand += 1
+  LetterDoorRotoren(x)
 
 #Printen van de gecodeerde tekst
-print("Dit is de omgezette tekst:")
-print(str(VertaaldeTekst))
+print("\nDit is de omgezette tekst:")
+print("".join(VertaaldeTekst))
