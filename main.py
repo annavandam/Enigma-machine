18ca5cb7dea772c22fe61bba665743b23e0afe00
Wouter Paardekooper <110270@st-maartenscollege.nl>
2020-05-26 12:59:54 +0000

    Er is een klas aangemaakt die de rotoren maakt. De rotoren worden daarna aangemaakt waardoor ze "aanwezig zijn". De rotoren kunnen nog geen acties doen.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 # maak hier de hele enigma machine in python :D
+alfabet = "abcdefghijklmnopqrstuvwxyz"
+
+class Rotor:
+  def __init__(self, nummer, lettervolgorde, beginstand):
+    self.nummer = nummer
+    self.beginstand = beginstand
+    self.lettervolgorde = lettervolgorde
+
+
+#Rotor 1 wordt aangemaakt met de class. Nummer van rotor, beginwaarde = het alfabet, beginwaarde kan worden ingevoerd. Dus hier met beginwaarde 2 , 0, 0
+rotor1 = Rotor(1, alfabet,  0)
+rotor2 = Rotor(2, alfabet,  0)
+rotor3 = Rotor(3, alfabet,  0)
