8384154d0d5e1190d556f1a2ff789efdd062bd6c
Wouter Paardekooper <110270@st-maartenscollege.nl>
2020-05-26 20:34:09 +0000

    De rotoren draaien. Rotor 1 draait bij elke letter. Rotor 2 draait bij een volledige ronde van 1 en rotor 3 draait bij een volledige ronde van rotor 2. De gecodeerde tekst wordt geprint.

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ VertaaldeTekst = []
 
 #De class van de rotoren, hiervan worden later drie objecten gemaakt omdat er drie rotoren zijn.
 class Rotor:
-  def __init__(self, nummer, lettervolgorde, beginstand):
+  def __init__(self, nummer, lettervolgorde, stand):
     self.nummer = nummer
-    self.beginstand = beginstand
+    self.stand = stand
     self.lettervolgorde = lettervolgorde
 
 #De rotoren worden hier aangemaakt.
@@ -14,25 +14,25 @@ rotor2 = Rotor(2, alfabet,  0)
 rotor3 = Rotor(3, alfabet,  0)
 
 #Hier wordt de gebruiker begeleid om de beginwaarden van de rotoren in te voeren en om de om te zetten tekst in te voeren. Verkeerde input wordt gemeld. Hierna kan de gebruiker zonder problemen de goede input invoeren.
-print("Voer de beginstanden van de rotoren in.")
+print("Voer de standen van de rotoren in.")
 while True:
   print("Stand rotor 1:")
   try:
-   rotor1.beginstand = int(input())
+   rotor1.stand = int(input())
    break
   except:
     print("Hier kunt u alleen getallen invoeren.")
 while True:
   print("Stand rotor 2:")
   try:
-   rotor2.beginstand = int(input())
+   rotor2.stand = int(input())
    break
   except:
     print("Hier kunt u alleen getallen invoeren.")
 while True:
   print("Stand rotor 3:")
   try:
-   rotor3.beginstand = int(input())
+   rotor3.stand = int(input())
    break
   except:
     print("Hier kunt u alleen getallen invoeren.")
@@ -49,10 +49,10 @@ def RotorDraai(draaiingen, rotor):
   rotor = (rotor.lettervolgorde[-draaiingen:] + rotor.lettervolgorde)[0: 26]
   return rotor
 
-#De rotoren worden gedraaid naar hun beginstand
-rotor1.lettervolgorde = RotorDraai(rotor1.beginstand, rotor1)
-rotor2.lettervolgorde = RotorDraai(rotor2.beginstand, rotor2)
-rotor3.lettervolgorde = RotorDraai(rotor3.beginstand, rotor3)
+#De rotoren worden gedraaid naar hun standbeginstand
+rotor1.lettervolgorde = RotorDraai(rotor1.stand, rotor1)
+rotor2.lettervolgorde = RotorDraai(rotor2.stand, rotor2)
+rotor3.lettervolgorde = RotorDraai(rotor3.stand, rotor3)
 
 
 
@@ -70,4 +70,22 @@ for x in TeVertalenLijst:
   x = alfabet[rotor2.lettervolgorde.index(x)]
   x = alfabet[rotor3.lettervolgorde.index(x)]
   VertaaldeTekst.append(x)
-  
+
+  #Rotor 1 draait één tikje.
+  rotor1.lettervolgorde = RotorDraai(1, rotor1)
+  rotor1.stand += 1
+
+  #Als rotor 1 een volledige draai heeft gemaakt, draait rotor 2 één tikje.
+  if rotor1.stand % 26 == 0:
+    rotor1.stand = 0
+    rotor2.lettervolgorde = RotorDraai(1, rotor2)
+    rotor2.stand += 1
+    #Als rotor 2 een volledige draai heeft gemaakt, draait rotor 3 één tikje.
+    if rotor2.stand % 26 == 0:
+      rotor2.stand = 0
+      rotor3.lettervolgorde = RotorDraai(1, rotor3)
+      rotor3.stand += 1
+
+#Printen van de gecodeerde tekst
+print("Dit is de omgezette tekst:")
+print(str(VertaaldeTekst))
